164eb767a5951650bde9ae2caec5027ca8480458
Sozzzaaa <sosa.santana.luisemiliano@gmail.com>
2025-09-21 23:10:18 -0600

    Aparentemente no es necesaria una linea...

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ function agregarAmigo()
 
 inputNewAmigo.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
-      event.preventDefault();   // stop default behavior
+      //event.preventDefault();   // stop default behavior
       botonAgregar.click();           // simulate button click
     }
   });
